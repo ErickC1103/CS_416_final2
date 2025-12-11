157a8ce16f3b47a4a9695f73a4b5f4729d67dc97
ErickC1103 <erickpatriciocardenas113003@gmail.com>
2025-12-10 21:57:04 -0500

    commit8

--- CS416_final/CS416_final/urls.py
+++ CS416_final/CS416_final/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path ,include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),#admin
     path('ticketmaster/', include('TicketMaster.urls'),name='ticketmaster'),#ticketmaster app
     path('accounts/', include('Accounts.urls'),name='accounts'),# account app
+# Redirect root URL to accounts homepage
+    path('', RedirectView.as_view(url='/accounts/', permanent=False)),
 ]
